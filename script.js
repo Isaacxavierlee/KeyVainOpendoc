643d37064113b072f80255f3d9b22fb67ebda367
Isaacxavierlee <101383340+Isaacxavierlee@users.noreply.github.com>
2023-09-30 07:06:54 +0000

    k

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("submitBtn").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  // Get input values
+  var name = document.getElementById("nameInput").value;
+  var email = document.getElementById("emailInput").value;
+  var subject = document.getElementById("subjectInput").value;
+  var message = document.getElementById("messageInput").value;
+  
+  // Perform validation or further processing if needed
+  
+  // Example: Display the input values in the console
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Subject:", subject);
+  console.log("Message:", message);
+  
+  // Clear input fields
+  document.getElementById("nameInput").value = "";
+  document.getElementById("emailInput").value = "";
+  document.getElementById("subjectInput").value = "";
+  document.getElementById("messageInput").value = "";
+});
